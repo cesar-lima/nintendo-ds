015bf404cbb11930208dd51ac0ddad8f48c7d29f
cesar-lima <cesarr340@gmail.com>
2024-06-04 19:21:24 -0300

    incluir funcionalidade do botão controls

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-function power() {
-    let gameScreen = document.getElementById('screenOne');
+let gameScreen = document.getElementById('screenOne');
+let controls = document.getElementById('popup-controls');
 
+function power() {
     if (gameScreen.style.display == 'none'){
         gameScreen.style.display = 'block';
         gameScreen.src = 'https://www.minijogos.com.br/embed/super-mario-world-online';
+        controls.style.display = 'none';
     } else {
         gameScreen.style.display = 'none';
         gameScreen.src = '';
+        controls.style.display = 'none';
+    }
+}
+
+function showcontrols() {
+    if (controls.style.display == 'none'){
+        gameScreen.style.display = 'none';
+        gameScreen.src = '';
+
+        controls.style.display = 'flex';
+    } else {
+        controls.style.display = 'none';
+
+        gameScreen.style.display = 'block';
+        gameScreen.src = 'https://www.minijogos.com.br/embed/super-mario-world-online';
     }
 }
